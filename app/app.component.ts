a4e46bc11b23808dc81b38a3bce7ad97c9b1ec7b
Alexandr Voroncov <me@roofav.xyz>
2016-11-29 23:36:37 +0700

    Upload project

---
+++ app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+const todos = [
+		{
+			title: 'Learn JavaScript',
+			completed: true
+		}, {
+			title: 'Learn Angular 2',
+			completed: false
+		}, {
+			title: 'Write app',
+			completed: false
+		}
+];
+
+@Component({
+	moduleId: module.id,
+	selector: 'app',
+	templateUrl: 'app.component.html',
+	styleUrls: ['app.component.css']
+})
+
+export class AppComponent {
+	title = 'Angular 2Do';
+	todos = todos;
+
+	toggle(todo: any) {
+		todo.completed = !todo.completed;
+	}
+
+	delete(todo: any) {
+		let index = this.todos.indexOf(todo);
+		if(index > -1) {
+			this.todos.splice(index, 1);
+		}
+	}
+}
