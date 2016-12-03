d1ac8d6cd410f9820b4568c254d01f1bbeede188
Alexandr Voroncov <me@roofav.xyz>
2016-12-03 22:29:28 +0700

    Upload project
    To add a task was created forms on the homepage.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 
-const todos = [
+class Todo {
+	constructor(public title: string,
+				public completed: boolean = false) {}
+}
+
+const todos: Todo[] = [
 		{
 			title: 'Learn JavaScript',
 			completed: true
@@ -21,14 +26,21 @@ const todos = [
 })
 
 export class AppComponent {
-	title = 'Angular 2Do';
-	todos = todos;
+	title: string = 'Angular 2Do';
+	todos: Todo[] = todos;
+	newTodoTitle: string = '';
 
-	toggle(todo: any) {
+	create() {
+		let todo: Todo = new Todo(this.newTodoTitle);
+		this.todos.push(todo);
+		this.newTodoTitle = '';
+	}
+
+	toggle(todo: Todo) {
 		todo.completed = !todo.completed;
 	}
 
-	delete(todo: any) {
+	delete(todo: Todo) {
 		let index = this.todos.indexOf(todo);
 		if(index > -1) {
 			this.todos.splice(index, 1);
